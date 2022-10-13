87f1de15eb31739032d50d8d9ba868c13980b83a
Gurjinder Singh <gurjindersingh663@gmail.com>
2022-10-13 22:54:35 +0530

    project created and assests added

---
+++ FruitsApp/FruitsApp/FruitsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsAppApp.swift
+//  FruitsApp
+//
+//  Created by Gurjinder Singh on 13/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
